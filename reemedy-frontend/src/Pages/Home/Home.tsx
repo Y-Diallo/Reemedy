f47833f2a42dff25464bdc0e1b6dbb5b521c4764
Disha Mahajan <disham2004@gmail.com>
2024-04-27 16:12:29 -0700

    formatted home page

--- reemedy-frontend/src/Pages/Home/Home.tsx
+++ reemedy-frontend/src/Pages/Home/Home.tsx
@@ -2,11 +2,11 @@ import { SetStateAction, useState } from "react";
 import SearchBar from "./components/SearchBar";
 import TypeSelector from "./components/TypeSelector";
 import IndianFlag from "../../assets/Flag_of_India.svg.png";
-import AmericanFlag from "../../assets/Flag_of_the_United_States_(DoS_ECA_Color_Standard).svg.png"
-import SouthAfricanFlag from "../../assets/Flag_of_South_Africa.svg.png"
-import MexicanFlag from "../../assets/Flag_of_Mexico.svg.png"
-import IndonesianFlag from "../../assets/Flag_of_Indonesia.svg.png"
-import JapaneseFlag from "../../assets/Flag_of_Japan.svg.png"
+import AmericanFlag from "../../assets/Flag_of_the_United_States_(DoS_ECA_Color_Standard).svg.png";
+import SouthAfricanFlag from "../../assets/Flag_of_South_Africa.svg.png";
+import MexicanFlag from "../../assets/Flag_of_Mexico.svg.png";
+import IndonesianFlag from "../../assets/Flag_of_Indonesia.svg.png";
+import JapaneseFlag from "../../assets/Flag_of_Japan.svg.png";
 
 function Home() {
   const [search, setSearch] = useState("");
@@ -29,28 +29,29 @@ function Home() {
   return (
     <>
       <div className="w-screen h-screen text-black flex flex-col">
-        <h1 className="text-3xl font-bold mt-0 ml-5 mt-10">Hello, {"Amanda"}</h1>
+        <h1 className="text-3xl font-bold mt-0 ml-5 mt-10">
+          Hello, {"Amanda"}
+        </h1>
         <SearchBar value={search} updateValue={doSearch} />
 
         <h2 className="text-1.5xl font-bold mt-10 ml-5">Types of Remedies</h2>
 
         <div className="flex mt-5 pb-5 hide-scroll-bar overflow-x-auto ml-3">
-  {countries.map((country) => (
-    <div key={country.id} className="inline-block px-2 text-center">
-      <div className="max-w-xs">
-        <div className="w-28 h-28 overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
-          <img
-            src={country.image}
-            alt={country.name}
-            className="w-full h-full object-cover"
-          />
+          {countries.map((country) => (
+            <div key={country.id} className="inline-block px-2 text-center">
+              <div className="max-w-xs">
+                <div className="w-28 h-28 overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                  <img
+                    src={country.image}
+                    alt={country.name}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <p className="mt-2 text-sm">{country.name}</p>
+              </div>
+            </div>
+          ))}
         </div>
-        <p className="mt-2 text-sm">{country.name}</p>
-      </div>
-    </div>
-  ))}
-</div>
-
 
         <TypeSelector />
         <h2 className="text-1.5xl font-bold mt-10 ml-5">Recommendations</h2>
