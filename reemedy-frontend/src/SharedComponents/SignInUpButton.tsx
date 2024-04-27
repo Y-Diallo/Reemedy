99e8698ff7b2e255de8d71ec41c871662e61fcd5
Disha Mahajan <disham2004@gmail.com>
2024-04-27 14:12:54 -0700

    added landing, signin, signup frontend code with routing

--- reemedy-frontend/src/SharedComponents/SignInUpButton.tsx
+++ reemedy-frontend/src/SharedComponents/SignInUpButton.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 type SignInUpButtonProps={
   proceed : ()=>void;
@@ -6,7 +8,7 @@ type SignInUpButtonProps={
 function SignInUpButton({proceed}:SignInUpButtonProps) {
   return (
     <button onClick={()=>proceed()}
-    className="w-10 rounded-2xl absolute right-1 bottom-1">👉</button>
+    className="w-10 bg-white text-black rounded-2xl absolute right-1 bottom-1 z-20"> <FontAwesomeIcon icon={faArrowRight} /></button>
   );
 }
 
