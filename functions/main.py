1c5d0d224d31d1a5e06ae5d382d248f200d49e39
Youssoupha Diallo <youssoupha.d72@gmail.com>
2024-04-27 23:48:44 -0700

    added chris changes

--- functions/main.py
+++ functions/main.py
@@ -1,14 +1,16 @@
 # Welcome to Cloud Functions for Firebase for Python!
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
-
+from dotenv import load_dotenv
 from datetime import datetime
+import os
+import json
 from firebase_functions import https_fn
 from firebase_admin import initialize_app, db
 import logging
 from openai import OpenAI
 
-client = OpenAI(api_key="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")
+client = OpenAI()
 assistant_id = "asst_li5Vl6s7S2hLldXVckc3hhis"
 
 logger = logging.getLogger('cloudfunctions.googleapis.com%2Fcloud-functions')
@@ -16,7 +18,7 @@ logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
 initialize_app()
-
+load_dotenv()
 
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
@@ -150,3 +152,70 @@ def do_chat_message(req: https_fn.CallableRequest) -> bool:
                 "timeStamp":datetime.now().isoformat()
             })
     return True
+
+@https_fn.on_call()
+def make_recommendation(req: https_fn.CallableRequest) -> list:
+    all_remedies_ref = db.reference("/remedies")
+    all_remedies = all_remedies_ref.get()
+    
+    user_ref = db.reference(f"users/{req.auth.uid}")
+    user = user_ref.get()
+    
+    prompt = ""
+    
+    if user:
+        ongoing_remedies = user.get("onGoingRemedies", [])
+        
+        remedy_info_list = []
+        for remedy_id, remedy_info in all_remedies.items():
+            logger.info(remedy_id)
+            remedy_name = remedy_info["name"]
+            remedy_description = remedy_info["description"]
+            remedy_benefits = remedy_info["benefits"]
+            list_of_benefits = ", ".join(remedy_benefits)
+            remedy_info_list.append(f"id: {remedy_id}, name: {remedy_name}, description: {remedy_description}, benefits: {list_of_benefits}")
+            
+        list_of_all_remedies = ", ".join(remedy_info_list)
+        logger.info(list_of_all_remedies)
+        
+        if ongoing_remedies:
+            list_of_remedies = ", ".join(ongoing_remedies)
+            prompt = (
+                f"I'm currently taking these natural remedies: {list_of_remedies}. "
+                f"From all of these options: {list_of_all_remedies}. "
+                "Please give me 3 recommendations for similar natural remedies. Please only return a list of the remedy id in this format (remedy1, remedy2, remedy3)"
+            )
+        else:
+            prompt = (
+                f"Please recommend me 3 natural remedies from this based on their remedyId: {list_of_all_remedies}. Please only return a list of the remedy id in this format (remedy1, remedy2, remedy3)"
+            )
+            
+        logger.info(prompt)
+        logger.info("HERE")
+        client = OpenAI(
+            api_key=os.environ.get("OPENAI_API_KEY"),
+        )
+        
+        completion = client.chat.completions.create(
+            model="gpt-4-turbo",
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
+        )       
+
+        contents = []
+        for choice in completion.choices:
+            content = choice.message.content
+            # Split the string by comma and remove leading/trailing whitespace and parentheses
+            remedies = [remedy.strip() for remedy in content.strip('()').split(',')]
+            contents.extend(remedies)
+
+        # Log the contents
+        logger.info(contents)
+
+        # Convert the list to a JSON-formatted string
+        contents_json = json.dumps(contents)
+
+        # Log the JSON-formatted string
+        logger.info(contents_json)
+        return contents_json
